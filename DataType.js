e7263e9222533bd11e212fc8aaae055bc08e541d
NamTaeHo <han127kr@gmail.com>
2024-05-01 11:53:29 +0900

    자바스크립트 데이터타입

---
+++ DataType.js
@@ -0,0 +1,10 @@
+const a = true, b = 1123.2, c = 'HELLO';
+
+console.log(a, b, c);
+console.log(typeof a, typeof b, typeof c);
+console.log(typeof(typeof a), typeof b, typeof c);
+
+let aa = '1', bb = '2';
+
+console.log(aa + bb);
+
---
+++ VarAndConst.js
@@ -0,0 +1,17 @@
+const S = 'HELLO';
+let person = '철수';
+
+console.log(S, person);
+
+person = '영희'
+
+console.log(S, person);
+
+
+let x = 1;
+let y = x;
+
+x = 10
+
+console.log(x,y);
+
